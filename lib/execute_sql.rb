c85174fc33822cee7e523272b0d8b487925486a2
metapsj <patrickjohnson@forty3sixteen.com>
2020-10-30 11:06:28 -0700

    implement sql execution

---
+++ lib/execute_sql.rb
@@ -0,0 +1,20 @@
+require 'sqlite3'
+
+module Migration
+  class ExecuteSql
+
+    def self.call
+      begin
+        db = SQLite3::Database.new ENV['DATABASE_PATH']
+        db.execute sql
+      rescue SQLite3::Exception => e
+        puts "Exception occurred"
+        puts e
+      ensure
+        db.close if db
+      end
+    end
+
+  end
+end
+
