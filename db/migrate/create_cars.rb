2cca8d3e765b5ee2d3b2fb75d3f29705bb126fd3
metapsj <patrickjohnson@forty3sixteen.com>
2020-10-29 18:17:15 -0700

    strip down migration to bare essentials

--- db/migrate/create_cars.rb
+++ db/migrate/create_cars.rb
@@ -1,12 +1,22 @@
-require './lib/migration'
+#!/usr/bin/env ruby
 
-sql = <<~DDL
+require 'sqlite3'
+
+DB = SQLite3::Database.new ENV['DATABASE_PATH']
+
+action = :up
+
+up = <<~DDL
   create table if not exists cars (
-    id integer primary key, 
-    label text, 
+    id integer primary key,
+    label text,
     price integer
-  )
+  );
 DDL
 
-DB.execute sql 
+down = <<~DDL
+  drop table cars;
+DDL
+
+DB.execute (action == :up ? up : down)
 
