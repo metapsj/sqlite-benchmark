ca9bce23aa3f838ce5cc535be2bd23590087b3a9
metapsj <patrickjohnson@forty3sixteen.com>
2020-10-30 10:28:06 -0700

    prototype a stripped down barebones migration

---
+++ script/proto-migration.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+action = :up
+
+up = <<~DDL
+  create table if not exists cars (
+    id integer primary key,
+    label text,
+    price integer
+  );
+DDL
+
+down = <<~DDL
+  drop table cars;
+DDL
+
+sql = (action == :up ? up : down)
+
+begin
+  db = SQLite3::Database.new ENV['DATABASE_PATH']
+  db.execute sql
+rescue SQLite3::Exception => e
+  puts "Exception occurred"
+  puts e
+ensure
+  db.close if db
+end
+
