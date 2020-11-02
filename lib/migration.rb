cb7615acd79a9ddd1521f0d762a416ccab0bd264
metapsj <patrickjohnson@forty3sixteen.com>
2020-11-02 13:37:23 -0800

    remove migration module / namespace

--- lib/migration.rb
+++ lib/migration.rb
@@ -1,40 +1,38 @@
 require 'sqlite3'
 
 module Migration
-  module Base
-    attr_reader :action, :sql
+  attr_reader :action, :sql
 
-    def initialize(action)
-      @action = action ||= :up
-      @sql = (action == :up ? up : down)
-    end
-
-    def up
-      raise "up method is not implemented"
-    end
-
-    def down
-      raise "down method is not implemented"
-    end
-
-    def run
-      execute
-    end
-
-    private
-
-    def execute
-      begin
-        db = SQLite3::Database.new ENV['DATABASE_PATH']
-        db.execute sql
-      rescue SQLite3::Exception => e
-        puts "Exception occurred"
-        puts e
-      ensure
-        db.close if db
-      end
-    end
-
+  def initialize(action)
+    @action = action ||= :up
+    @sql = (action == :up ? up : down)
   end
+
+  def up
+    raise "up method is not implemented"
+  end
+
+  def down
+    raise "down method is not implemented"
+  end
+
+  def run
+    execute
+  end
+
+  private
+
+  def execute
+    begin
+      db = SQLite3::Database.new ENV['DATABASE_PATH']
+      db.execute sql
+    rescue SQLite3::Exception => e
+      puts "Exception occurred"
+      puts e
+    ensure
+      db.close if db
+    end
+  end
+
 end
 
