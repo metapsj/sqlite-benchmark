310ee6e157b79f6f32819fb09f5a14decb6f07e6
metapsj <patrickjohnson@forty3sixteen.com>
2020-11-02 13:41:21 -0800

    refactor execute sql to maintain connection

--- lib/execute_sql.rb
+++ lib/execute_sql.rb
@@ -1,20 +1,35 @@
 require 'sqlite3'
 
-module Migration
-  class ExecuteSql
+class ExecuteSql
+  attr_reader :connection_string
 
-    def self.call(sql)
-      begin
-        db = SQLite3::Database.new ENV['DATABASE_PATH']
-        db.execute sql
-      rescue SQLite3::Exception => e
-        puts "Exception occurred"
-        puts e
-      ensure
-        db.close if db
-      end
-    end
-
+  def initialize(connection_string)
+    @connection_string = connection_string
   end
+
+  def call(sql)
+    begin
+      db.execute sql
+    rescue SQLite3::Exception => e
+      puts "Exception Occurred"
+      puts e
+    ensure
+      db.close if db
+    end
+  end
+
+  private 
+
+  def connect
+    begin
+      db = SQLite3::Database.new connection_string
+    rescue SQLite3::Exception => e
+      puts "Exception Occurred"
+      puts e
+    ensure
+      db.close if db
+    end
+  end
+
 end
 
