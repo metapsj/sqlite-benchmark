4b495cb428e09b6c18c29ec7ef004605ac74b004
metapsj <patrickjohnson@forty3sixteen.com>
2020-11-02 12:47:35 -0800

    renamed create cars to create prices.  change definition for the table.

--- db/migrate/create_cars.rb
+++ db/migrate/create_cars.rb
@@ -2,22 +2,23 @@
 
 require './lib/migration'
 
-class CreateCars
+class CreatePrices
   include Migration::Base
 
   def up
     <<~DDL
-      create table if not exists cars (
-        id integer primary key,
-        label text,
-        price integer
+      create table if not exists prices (
+        price_id integer not null primary key autoincrement,
+        item_number text not null unique,
+        amount integer not null,
+        created_at text not null default current_timestamp
       );
     DDL
   end
 
   def down
     <<~DDL
-      drop table cars;
+      drop table prices;
     DDL
   end
 end
