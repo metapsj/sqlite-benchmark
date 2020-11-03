27d20c2710c37cbec85fd9da8d6b93467ed6cfce
metapsj <patrickjohnson@forty3sixteen.com>
2020-11-02 16:33:01 -0800

    fix execute sql

--- lib/execute_sql.rb
+++ lib/execute_sql.rb
@@ -1,10 +1,19 @@
 require 'sqlite3'
 
 class ExecuteSql
-  attr_reader :connection_string
+  attr_reader :connection_string, :db
 
   def initialize(connection_string)
     @connection_string = connection_string
+  end
+
+  def open
+    begin
+      @db = SQLite3::Database.new connection_string
+    rescue SQLite3::Exception => e
+      puts "Exception Occurred"
+      puts e
+    end
   end
 
   def call(sql)
@@ -13,23 +22,11 @@ class ExecuteSql
     rescue SQLite3::Exception => e
       puts "Exception Occurred"
       puts e
-    ensure
-      db.close if db
     end
   end
 
-  private 
-
-  def connect
-    begin
-      db = SQLite3::Database.new connection_string
-    rescue SQLite3::Exception => e
-      puts "Exception Occurred"
-      puts e
-    ensure
-      db.close if db
-    end
+  def close
+    db.close if db
   end
-
 end
 
