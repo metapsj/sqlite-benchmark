93d259474a57f212257a7e1e5422b4e279715f99
metapsj <patrickjohnson@forty3sixteen.com>
2020-11-03 15:37:08 -0800

    implement direct reads benchmark

--- test/benchmark/direct_reads.rb
+++ test/benchmark/direct_reads.rb
@@ -1,10 +1,68 @@
+#!/usr/bin/env ruby
+
 require 'benchmark'
+require './lib/execute_sql'
 
-n = 5000000
+@execute_sql = ExecuteSql.new(ENV['DATABASE_PATH'])
 
-Benchmark.bm(7) do |x|
-  x.report("for:") { for i in 1..n; a = "1"; end }
-  x.report("times:") { n.times do; a = "1"; end }
-  x.report("upto:") { 1.upto(n) do; a = "1"; end }
+def setup
+  @execute_sql.open if @execute_sql
+
+  @execute_sql.call <<~DDL
+    PRAGMA journal_mode=WAL;
+  DDL
+
+  @execute_sql.call <<~DDL
+    create table if not exists prices (
+      price_id text not null primary key,
+      item_number text not null unique,
+      price integer not null default 0,
+      created_at text not null default current_timestamp
+    );
+  DDL
 end
+
+def teardown
+  @execute_sql.call <<~DDL
+    drop table prices;
+  DDL
+
+  @execute_sql.close if @execute_sql
+end
+
+def load_prices(filename)
+  fixture_path = File.expand_path(ENV['TEST_FIXTURE_PATH'])
+  filepath = File.join(fixture_path, filename)
+  File.open(filepath, "r").map{ |line| line.chomp.split(", ") }
+end
+
+def exercise(prices)
+  prices.map{ |line| line.first }.each do |price_id|
+    @execute_sql.call <<~DML
+      select  price_id,
+              item_number,
+              price,
+              created_at
+      from    prices 
+      where   price_id = '#{price_id}';
+    DML
+  end
+end
+
+def benchmark
+  Benchmark.bm(7) do |x|
+    prices = load_prices("prices-100.csv")
+    x.report("100:") { exercise(prices) }
+
+    prices = load_prices("prices-1K.csv")
+    x.report("1K:") { exercise(prices) }
+
+    prices = load_prices("prices-10K.csv")
+    x.report("10K:") { exercise(prices) }
+  end
+end
+
+setup
+benchmark
+teardown
 
