1d76b9784cb99948ef18bee6135db8bf2bbe9646
metapsj <patrickjohnson@forty3sixteen.com>
2020-11-03 13:44:50 -0800

    implement fixture based loading of prices to exercise database

--- test/benchmark/direct_writes.rb
+++ test/benchmark/direct_writes.rb
@@ -1,9 +1,6 @@
 #!/usr/bin/env ruby
 
 require 'benchmark'
-require 'securerandom'
-require 'time'
-require 'digest/murmurhash'
 require './lib/execute_sql'
 
 @execute_sql = ExecuteSql.new(ENV['DATABASE_PATH'])
@@ -23,6 +20,8 @@ def setup
       created_at text not null default current_timestamp
     );
   DDL
+
+  # load
 end
 
 def teardown
@@ -33,19 +32,20 @@ def teardown
   @execute_sql.close
 end
 
-def exercise(frequency)
-  frequency.times do |count|
-    price_id = SecureRandom.uuid
-    created_at = Time.now.iso8601
-    price = rand(0.47..100.00).round(2)
-    item_number = Digest::MurmurHash64A.hexdigest("#{price_id}-#{price}-#{created_at}-#{count}")
+def load_prices(filename)
+  fixture_path = File.expand_path(ENV['TEST_FIXTURE_PATH'])
+  filepath = File.join(fixture_path, filename)
+  prices = File.open(filepath, "r").map { |line| line.chomp.split(", ") }
+end
 
+def exercise(prices)
+  prices.each do |line| 
     @execute_sql.call <<~DML
       insert into prices values (
-        '#{price_id}',
-        '#{item_number}',
-        #{price},
-        '#{created_at}'
+        '#{line[0]}', 
+        '#{line[1]}', 
+        #{line[2]}, 
+        '#{line[3]}'
       );
     DML
   end
@@ -53,9 +53,14 @@ end
 
 def benchmark
   Benchmark.bm(7) do |x|
-    x.report("100:") { exercise(100) }
-    x.report("1K:") { exercise(1000) }
-    x.report("10K:") { exercise(10000) }
+    prices = load_prices("prices-100.csv")
+    x.report("100:") { exercise(prices) }
+
+    prices = load_prices("prices-1K.csv")
+    x.report("1K:") { exercise(prices) }
+
+    prices = load_prices("prices-10K.csv")
+    x.report("10K:") { exercise(prices) }
   end
 end
 
