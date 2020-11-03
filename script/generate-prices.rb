bae2dde32417ce9f0fe74c6e5996796111c37065
metapsj <patrickjohnson@forty3sixteen.com>
2020-11-03 11:20:48 -0800

    implement generate prices

--- script/generate-prices.rb
+++ script/generate-prices.rb
@@ -4,15 +4,30 @@ require 'securerandom'
 require 'time'
 require 'digest/murmurhash'
 
-# 100, 1000, 10000
-frequency = 1000
+CSV_PATH = File.expand_path(ENV['TEST_FIXTURE_PATH'])
 
-frequency.times do |count|
-  price_id = SecureRandom.uuid
-  created_at = Time.now.iso8601
-  price = rand(0.47..100.00).round(2)
-  item_number = Digest::MurmurHash64A.hexdigest("#{price_id}-#{price}-#{created_at}-#{count}")
+files = [
+  { filename: "prices-100.csv", frequency: 100 },
+  { filename: "prices-1K.csv", frequency: 1000 },
+  { filename: "prices-10K.csv", frequency: 10000 },
+]
 
-  "#{price_id}, #{item_number}, #{price}, #{created_at}"
+def generate_csv(filename, frequency)
+  csv = File.open(File.join(CSV_PATH, filename), "a")
+
+  frequency.times do |count|
+    price_id = SecureRandom.uuid
+    created_at = Time.now.iso8601
+    price = rand(0.47..100.00).round(2)
+    item_number = Digest::MurmurHash64A.hexdigest("#{price_id}-#{price}-#{created_at}-#{count}")
+
+    csv.write("#{price_id}, #{item_number}, #{price}, #{created_at}\n")
+  end
+
+  csv.close if csv
 end
+
+files
+  .map(&:values)
+  .each{ |filename, frequency| generate_csv(filename, frequency) }
 
