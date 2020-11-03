42c58645696ccf87cfcf367a219b1ee270d3ff05
metapsj <patrickjohnson@forty3sixteen.com>
2020-11-02 16:34:41 -0800

    implement basic benchmark for discrete writes.  note: average insert per second is ~240 as of this implementation

--- benchmark/discrete_write.rb
+++ benchmark/discrete_write.rb
@@ -1,10 +1,58 @@
+#!/usr/bin/env ruby
+
 require 'benchmark'
+require 'securerandom'
+require 'time'
+require 'digest/murmurhash'
+require './lib/execute_sql'
 
-n = 5000000
+@execute_sql = ExecuteSql.new(ENV['DATABASE_PATH'])
+@execute_sql.open
 
-Benchmark.bm(7) do |x|
-  x.report("for:") { for i in 1..n; a = "1"; end }
-  x.report("times:") { n.times do; a = "1"; end }
-  x.report("upto:") { 1.upto(n) do; a = "1"; end }
+def setup
+  @execute_sql.call <<~DDL
+    create table if not exists prices (
+      price_id text not null primary key,
+      item_number text not null unique,
+      price integer not null default 0,
+      created_at text not null default current_timestamp
+    );
+  DDL
 end
+
+def teardown
+  @execute_sql.call <<~DDL
+    drop table prices;
+  DDL
+end
+
+def exercise(frequency)
+  frequency.times do |count|
+    price_id = SecureRandom.uuid
+    created_at = Time.now.iso8601
+    price = rand(0.47..100.00).round(2)
+    item_number = Digest::MurmurHash64A.hexdigest("#{price_id}-#{price}-#{created_at}-#{count}")
+    
+    @execute_sql.call <<~DML
+      insert into prices values (
+        '#{price_id}', 
+        '#{item_number}', 
+        #{price}, 
+        '#{created_at}'
+      );
+    DML
+  end
+end
+
+def benchmark
+  Benchmark.bm(7) do |x|
+    x.report("100:") { exercise(100) }
+    x.report("1K:") { exercise(1000) }
+    x.report("10K:") { exercise(10000) }
+  end
+end
+
+setup
+benchmark
+# teardown
 
