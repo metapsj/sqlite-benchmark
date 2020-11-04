5aebc8cf00dd53d085a26a7f48a32fc05412ad53
metapsj <patrickjohnson@forty3sixteen.com>
2020-11-03 23:24:40 -0800

    reimplement direct writes benchmark

--- test/benchmark/direct_writes.rb
+++ test/benchmark/direct_writes.rb
@@ -1,68 +1,73 @@
 #!/usr/bin/env ruby
 
-require 'benchmark'
+require './lib/benchmark'
 require './lib/execute_sql'
 
-@execute_sql = ExecuteSql.new(ENV['DATABASE_PATH'])
+class DirectWrites
+  include Benchmark::Base
 
-def setup
-  @execute_sql.open if @execute_sql
+  def setup
+    @execute_sql = ExecuteSql.new(ENV['DATABASE_PATH'])
 
-  @execute_sql.call <<~DDL
-    PRAGMA journal_mode=WAL;
-  DDL
+    @execute_sql.open if @execute_sql
 
-  @execute_sql.call <<~DDL
-    create table if not exists prices (
-      price_id text not null primary key,
-      item_number text not null unique,
-      price integer not null default 0,
-      created_at text not null default current_timestamp
-    );
-  DDL
-end
+    @execute_sql.call <<~DDL
+      PRAGMA journal_mode=WAL;
+    DDL
 
-def teardown
-  @execute_sql.call <<~DDL
-    drop table prices;
-  DDL
-
-  @execute_sql.close if @execute_sql
-end
-
-def load_prices(filename)
-  fixture_path = File.expand_path(ENV['TEST_FIXTURE_PATH'])
-  filepath = File.join(fixture_path, filename)
-  prices = File.open(filepath, "r").map { |line| line.chomp.split(", ") }
-end
-
-def exercise(prices)
-  prices.each do |line|
-    @execute_sql.call <<~DML
-      insert into prices values (
-        '#{line[0]}',
-        '#{line[1]}',
-        #{line[2]},
-        '#{line[3]}'
+    @execute_sql.call <<~DDL
+      create table if not exists prices (
+        price_id text not null primary key,
+        item_number text not null unique,
+        price integer not null default 0,
+        created_at text not null default current_timestamp
       );
-    DML
+    DDL
   end
+
+  def teardown
+    @execute_sql.call <<~DDL
+      drop table prices;
+    DDL
+
+    @execute_sql.close if @execute_sql
+  end
+
+  def exercise(prices)
+    prices.each do |line|
+      @execute_sql.call <<~DML
+        insert into prices values (
+          '#{line[0]}',
+          '#{line[1]}',
+          #{line[2]},
+          '#{line[3]}'
+        );
+      DML
+    end
+  end
+
+  def benchmark
+    Benchmark.bm(7) do |x|
+      prices = load_prices("prices-100.csv")
+      x.report("100:") { exercise(prices) }
+
+      prices = load_prices("prices-1K.csv")
+      x.report("1K:") { exercise(prices) }
+
+      prices = load_prices("prices-10K.csv")
+      x.report("10K:") { exercise(prices) }
+    end
+  end
+
+  private
+
+  def load_prices(filename)
+    fixture_path = File.expand_path(ENV['TEST_FIXTURE_PATH'])
+    filepath = File.join(fixture_path, filename)
+    prices = File.open(filepath, "r").map { |line| line.chomp.split(", ") }
+  end
+
 end
 
-def benchmark
-  Benchmark.bm(7) do |x|
-    prices = load_prices("prices-100.csv")
-    x.report("100:") { exercise(prices) }
-
-    prices = load_prices("prices-1K.csv")
-    x.report("1K:") { exercise(prices) }
-
-    prices = load_prices("prices-10K.csv")
-    x.report("10K:") { exercise(prices) }
-  end
-end
-
-setup
-benchmark
-teardown
+DirectWrites.new.run
 
