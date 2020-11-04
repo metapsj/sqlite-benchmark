4b56ff9f961ce728bbc359cf04124350f16b2a8c
metapsj <patrickjohnson@forty3sixteen.com>
2020-11-04 15:56:38 -0800

    move migration script elements into dedicated script

---
+++ script/db-migrate-schema.rb
@@ -0,0 +1,11 @@
+#!/bin/usr/env ruby
+
+require './db/migrate/enable_wal'
+require './db/migrate/enable_synchronous'
+require './db/migrate/create_prices'
+
+EnableWal.new(:up).call
+EnableSynchronous.new(:up).call
+
+CreatePrices.new(:up).call
+
