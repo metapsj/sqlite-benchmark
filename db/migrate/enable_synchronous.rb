32564533ea17d8386b4c7c404ef3e27da987cd33
metapsj <patrickjohnson@forty3sixteen.com>
2020-11-04 11:58:18 -0800

    add enable synchronous to migrations

---
+++ db/migrate/enable_synchronous.rb
@@ -0,0 +1,25 @@
+#!/bin/usr/env ruby
+
+require './lib/migration'
+
+class EnableSynchronous
+  include Migration
+
+  def up
+    <<~DDL
+      pragma synchronous=NORMAL;
+    DDL
+  end
+
+  def down
+    <<~DDL
+      pragma synchronous=FULL;
+    DDL
+      
+    DDL
+  end
+
+end
+
+EnableSynchronous.new(:up).run
+
