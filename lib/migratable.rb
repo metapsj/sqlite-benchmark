48b7250710500562a1967dc5f03e2c1568a7ecab
metapsj <patrickjohnson@forty3sixteen.com>
2020-11-05 17:26:15 -0800

    refactor migratable

--- lib/migratable.rb
+++ lib/migratable.rb
@@ -1,32 +1,57 @@
 require './lib/execute_sql'
 
 module Migratable
-  attr_reader :action, :sql
+  def self.included(cls)
+    cls.class_exec do
+      include Migrate
+      include Execute
+      extend Build
+      prepend Configure
 
-  def initialize(action)
-    @action = action ||= :up
-    @sql = (action == :up ? up : down)
+      alias_method :call, :execute
+    end
   end
 
-  def up
-    raise "up method is not implemented"
+  module Build
+    def build(action=nil)
+      action ||= :up
+      instance = new
+      instance.configure(action: :up)
+      instance
+    end
+
+    def call
+      instance = build
+      instance.call
+    end
   end
 
-  def down
-    raise "down method is not implemented"
+  module Migrate
+    def up
+      raise "Migrate#up has not been implemented"
+    end
+
+    def down
+      raise "Migrate#down has not been implemented"
+    end
   end
 
-  def call
-    execute
+  module Configure
+    def configure(**kwargs)
+      action = kwargs.fetch(:action, :up)
+      sql = (action == :up ? up : down)
+    end
   end
+  
+  module Execute
+    attr_accessor :action, :sql
 
-  private
-
-  def execute
-    execute_sql = ExecuteSql.new(ENV['DATABASE_PATH'])
-    execute_sql.open if execute_sql
-    execute_sql.call(sql)
-    execute_sql.close if execute_sql
+    def execute
+      execute_sql = ExecuteSql.new(ENV['DATABASE_PATH'])
+      execute_sql.open if execute_sql
+      execute_sql.call(sql)
+      execute_sql.close if execute_sql
+    end
   end
 end
 
